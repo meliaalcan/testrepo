79f7542c9a43442b5c250bd66fe75d602ed37cc6
meliaalcan <72538111+meliaalcan@users.noreply.github.com>
2021-01-10 22:49:42 -0500

    hello

--- wp-login.php
+++ wp-login.php
@@ -4,11 +4,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Connect Tutoring &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/wp-includes/css/dashicons.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/wp-includes/css/buttons.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/wp-admin/css/forms.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/wp-admin/css/l10n.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/wp-admin/css/login.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://meliaalcan.github.io/testrepo/wp-includes/css/dashicons.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://meliaalcan.github.io/testrepo/wp-includes/css/buttons.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://meliaalcan.github.io/testrepo/wp-admin/css/forms.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://meliaalcan.github.io/testrepo/wp-admin/css/l10n.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://meliaalcan.github.io/testrepo/wp-admin/css/login.min.css?ver=5.6' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://meliaalcan.github.io/testrepo/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -38,34 +38,34 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-									<input type="hidden" name="redirect_to" value="http://localhost/connecttutoring.ca/wp-admin/" />
+									<input type="hidden" name="redirect_to" value="https://meliaalcan.github.io/testrepo/wp-admin/" />
 									<input type="hidden" name="testcookie" value="1" />
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="https://meliaalcan.github.io/testrepo/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/">
+				<p id="backtoblog"><a href="https://meliaalcan.github.io/testrepo/">
 		&larr; Go to Connect Tutoring		</a></p>
 			</div>
-	<script type='text/javascript' src='/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
-<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+	<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
 <script type='text/javascript' id='zxcvbn-async-js-extra'>
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"http:\/\/localhost\/connecttutoring.ca\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/meliaalcan.github.io\/testrepo\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
-<script type='text/javascript' src='/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
 <script type='text/javascript' id='wp-polyfill-js-after'>
-( 'fetch' in window ) || document.write( '<script src="http://localhost/connecttutoring.ca/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="http://localhost/connecttutoring.ca/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="http://localhost/connecttutoring.ca/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="http://localhost/connecttutoring.ca/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="http://localhost/connecttutoring.ca/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="http://localhost/connecttutoring.ca/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
+( 'fetch' in window ) || document.write( '<script src="https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://meliaalcan.github.io/testrepo/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
 </script>
-<script type='text/javascript' src='/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909' id='wp-i18n-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909' id='wp-i18n-js'></script>
 <script type='text/javascript' id='password-strength-meter-js-extra'>
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
@@ -78,14 +78,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type='text/javascript' src='/wp-admin/js/password-strength-meter.min.js?ver=5.6' id='password-strength-meter-js'></script>
-<script type='text/javascript' src='/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-admin/js/password-strength-meter.min.js?ver=5.6' id='password-strength-meter-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
 <script type='text/javascript' id='wp-util-js-extra'>
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/connecttutoring.ca\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-includes/js/wp-util.min.js?ver=5.6' id='wp-util-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-includes/js/wp-util.min.js?ver=5.6' id='wp-util-js'></script>
 <script type='text/javascript' id='user-profile-js-translations'>
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
@@ -93,7 +93,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/connecttutoring.ca\/wp-admin\/admin-ajax
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type='text/javascript' src='/wp-admin/js/user-profile.min.js?ver=5.6' id='user-profile-js'></script>
+<script type='text/javascript' src='https://meliaalcan.github.io/testrepo/wp-admin/js/user-profile.min.js?ver=5.6' id='user-profile-js'></script>
 	<div class="clear"></div>
 	</body>
 	</html>
